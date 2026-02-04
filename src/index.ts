ac2a7300aa5fac1feb470e08d477a0538af0695a
ibrahim galal <ibrahimgalal2011@gmail.com>
2026-02-04 07:17:30 +0200

    update and add read.md

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,12 @@ import express, { Request, Response } from "express";
 const app = express();
 const PORT = 8000;
 
-// Middleware to parse JSON
 app.use(express.json());
 
-// Root GET route
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello ,welcome to the classroom API!" });
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
